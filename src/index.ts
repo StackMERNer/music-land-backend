bd09034a5b925f88eac71afe2f0d954756e8fcf0
Md Ashraful Islam <ms785314@gmail.com>
2024-02-26 21:52:24 +0600

    create home route

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,22 @@
-// import cors from "cors";
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import categoriesRouter from "./routes/categories";
-import instrumentsRoute from "./routes/instruments";
+import instrumentsRouter from "./routes/instruments";
+import homeRouter from "./routes/home";
 const app = express();
 const port = 5000;
 dotenv.config();
 app.use(cors());
-
+app.use("/", homeRouter);
 app.use("/categories", categoriesRouter);
-app.use("/instruments", instrumentsRoute);
-// Middleware function to log incoming requests
+app.use("/instruments", instrumentsRouter);
 
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Express server!");
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
